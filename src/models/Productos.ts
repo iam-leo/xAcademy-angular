ea193ebda9d96c52839239be639559b126bc97b7
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-07-12 16:37:30 -0300

    add: product interface

---
+++ src/models/Productos.ts
@@ -0,0 +1,9 @@
+export class Producto{
+    nombre: string;
+    costo: string;
+
+    constructor(nombre: string, costo: string){
+        this.nombre = nombre;
+        this.costo = costo;
+    }
+}
